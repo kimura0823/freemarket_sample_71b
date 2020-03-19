b07ae539294fbeaaef25e0bcef6e805cb5c0b628
DON4024 <61001693+DON4024@users.noreply.github.com>
2020-03-19 12:59:36 +0900

    update user.rb
    password, email validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,10 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  before_save { self.email = email.downcase }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
+
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :first_name_kana, presence: true
@@ -12,6 +16,13 @@ class User < ApplicationRecord
   validates :birthday_m, presence: true
   validates :birthday_d, presence: true
   validates :nickname, presence: true, uniqueness: true
-  validates :email, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: true、format: { with: VALID_EMAIL_REGEX }
   validates :encrypted_password, presence: true, uniqueness: true
+
+  has_many :products
+  has_one :address
+  has_one :credit
+
+  
+  format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
 end
