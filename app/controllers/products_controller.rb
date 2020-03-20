cd54aa9e8c01d20325fb76a30761562ea5269dd5
kimura0823 <61189824+kimura0823@users.noreply.github.com>
2020-03-20 21:19:06 +0900

    Revert "active_hash"
    This reverts commit aa5898b33d497dca7bd3571f45089c1c48cbd2e0.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -7,26 +7,10 @@ class ProductsController < ApplicationController
   def new
     @product = Product.new
     @product.images.new
-
-      #セレクトボックスの初期値設定
-    @category_parent_array = ["---"]
-    Category.where(ancestry: nil).each do |parent|
-      @category_parent_array << parent.name
-    end
-  end
-  
-  def get_category_children
-    #選択された親カテゴリーに紐付く子カテゴリーの配列を取得
-    @category_children = Category.find_by(name: "#{params[:parent_name]}", ancestry: nil).children
-  end
-
-    # 子カテゴリーが選択された後に動くアクション
-  def get_category_grandchildren
-  　　　#選択された子カテゴリーに紐付く孫カテゴリーの配列を取得
-    @category_grandchildren = Category.find("#{params[:child_id]}").children
   end
   
   def create
+
     @product = Product.new(product_params)
     if @product.save
       redirect_to root_path
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,5 @@
 Rails.application.routes.draw do
   root 'products#index'
-  resources :products do
-    #Ajaxで動くアクションのルートを作成
-    collection do
-      get 'get_category_children', defaults: { format: 'json' }
-      get 'get_category_grandchildren', defaults: { format: 'json' }
-    end
-  end
+  resources :products, except: :show
 
 end
