aa5898b33d497dca7bd3571f45089c1c48cbd2e0
kimura0823 <61189824+kimura0823@users.noreply.github.com>
2020-03-20 21:12:45 +0900

    active_hash

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   root 'products#index'
-  resources :products, except: :show
+  resources :products do
+    #Ajaxで動くアクションのルートを作成
+    collection do
+      get 'get_category_children', defaults: { format: 'json' }
+      get 'get_category_grandchildren', defaults: { format: 'json' }
+    end
+  end
 
 end
