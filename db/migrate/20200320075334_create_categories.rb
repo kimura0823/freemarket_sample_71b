94a792afbe45e4ad05dd17d1668d5aa2b1f77193
DON4024 <61001693+DON4024@users.noreply.github.com>
2020-03-22 10:01:34 +0900

    put initial-data into categories

--- db/migrate/20200320075334_create_categories.rb
+++ db/migrate/20200320075334_create_categories.rb
@@ -2,7 +2,7 @@ class CreateCategories < ActiveRecord::Migration[5.2]
   def change
     create_table :categories do |t|
       t.string :name, null: false
-      t.integer :ancestry, foreign_key:true
+      t.string :ancestry, foreign_key:true
       t.integer :size_tag
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -26,7 +26,7 @@ ActiveRecord::Schema.define(version: 2020_03_20_075334) do
 
   create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
-    t.integer "ancestry"
+    t.string "ancestry"
     t.integer "size_tag"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
