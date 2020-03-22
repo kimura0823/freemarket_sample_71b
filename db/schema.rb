ca7edb64ea40e3ae3769d8dd8362487c7c288f31
DON4024 <61001693+DON4024@users.noreply.github.com>
2020-03-22 20:10:55 +0900

    add schema about users-table

--- db/schema.rb
+++ db/schema.rb
@@ -22,6 +22,27 @@ ActiveRecord::Schema.define(version: 2020_03_20_075334) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "first_name", null: false
+    t.string "last_name", null: false
+    t.string "first_name_kana", null: false
+    t.string "last_name_kana", null: false
+    t.string "birthday_y", null: false
+    t.string "birthday_m", null: false
+    t.string "birthday_d", null: false
+    t.string "nickname", null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+  
+
   create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
     t.string "ancestry"
