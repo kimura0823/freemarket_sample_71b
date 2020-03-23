9c628ab40b36b6dc3ac701e440c317ac186b20e4
kimura0823 <61189824+kimura0823@users.noreply.github.com>
2020-03-23 18:48:24 +0900

    3/23.view

--- db/schema.rb
+++ db/schema.rb
@@ -46,7 +46,6 @@ ActiveRecord::Schema.define(version: 2020_03_20_075334) do
     t.string "brand"
     t.string "status", null: false
     t.integer "price", null: false
-    t.integer "judge", null: false
     t.integer "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
