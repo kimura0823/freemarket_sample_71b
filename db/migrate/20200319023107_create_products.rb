6e3f93452e38e06302810d9fada898e16d120c8f
DON4024 <61001693+DON4024@users.noreply.github.com>
2020-03-23 13:22:19 +0900

    update product-table

--- db/migrate/20200319023107_create_products.rb
+++ db/migrate/20200319023107_create_products.rb
@@ -5,10 +5,12 @@ class CreateProducts < ActiveRecord::Migration[5.2]
       t.string :description, null: false
       t.integer :category_id, null: false, foreign_key: true
       t.string :brand
-      t.string :status, null: false
       t.integer :price, null: false
       t.integer :judge, null: false
       t.integer :user_id, null: false, foreign_key: true
+      t.integer :burden_id, null: false
+      t.integer :days_id, null: false
+      t.integer :status_id, null: false
       t.timestamps
     end
   end
