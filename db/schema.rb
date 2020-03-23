b715a18b78668a4180f2fb80bfbb1170c8721da2
DON4024 <61001693+DON4024@users.noreply.github.com>
2020-03-23 14:25:12 +0900

    fix schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 2020_03_20_075334) do
-
+  
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "postcode", null: false
     t.integer "prefecture_id", null: false
@@ -71,5 +71,5 @@ ActiveRecord::Schema.define(version: 2020_03_20_075334) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
-
+  
 end
