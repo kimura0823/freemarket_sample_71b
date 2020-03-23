ba5ff2954504c65e756ef2da132ea23cae9861a6
DON4024 <61001693+DON4024@users.noreply.github.com>
2020-03-23 12:38:38 +0900

    use dammy items#new

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
     post 'addresses', to: 'users/registrations#create_address'
   end
   root 'items#index'
+  resources :items
   resources :products do
     #Ajaxで動くアクションのルートを作成
     collection do
