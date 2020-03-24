0afda767e67a1f4cc4c53091a01b61385900bd89
DON4024 <61001693+DON4024@users.noreply.github.com>
2020-03-24 14:39:59 +0900

    delete comment-out

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -8,7 +8,6 @@ class ProductsController < ApplicationController
     @product = Product.new
     @product.images.new
 
-      #セレクトボックスの初期値設定
     @category_parent_array = ["---"]
     Category.where(ancestry: nil).each do |parent|
       @category_parent_array << parent.name
@@ -16,19 +15,14 @@ class ProductsController < ApplicationController
   end
   
   def get_category_children
-    binding.pry
-    #選択された親カテゴリーに紐付く子カテゴリーの配列を取得
     @category_children = Category.find_by(name: "#{params[:parent_name]}", ancestry: nil).children
   end
 
-    # 子カテゴリーが選択された後に動くアクション
   def get_category_grandchildren
-    #選択された子カテゴリーに紐付く孫カテゴリーの配列を取得
     @category_grandchildren = Category.find("#{params[:child_id]}").children
   end
   
   def create
-    binding.pry
     @product = Product.new(product_params)
     categoryId_params
     if @product.save 
@@ -56,9 +50,6 @@ class ProductsController < ApplicationController
   private
 
   def product_params
-    # params.require(:product).permit(:name, :price, :description, :status, :user_id, images_attributes: [:image])
-
-    # #ログインできるようになったらこちらに変える
     params.require(:product).permit(:name, :price, :description, :status_id, :brand, :burden_id, :days_id, :prefecture_id, images_attributes: [:image]).merge(user_id: current_user.id)
   end
 
