2cc507a50451eaf05cf71481deeac8ab768c3c15
shibatan1234 <61174383+shibatan1234@users.noreply.github.com>
2020-03-26 14:40:02 +0900

    create-card2

--- app/assets/javascripts/payjp.js
+++ app/assets/javascripts/payjp.js
@@ -1,7 +1,7 @@
 document.addEventListener(
   "DOMContentLoaded", e => {
     if (document.getElementById("token_submit") != null) { //token_submitというidがnullの場合、下記コードを実行しない
-      Payjp.setPublicKey("pk_test_afab671374c1ccc48fda7c3f"); //ここに公開鍵を直書き
+      Payjp.setPublicKey("pk_test_0383a1b8f91e8a6e3ea0e2a9"); //ここに公開鍵を直書き
       let btn = document.getElementById("token_submit"); //IDがtoken_submitの場合に取得されます
       btn.addEventListener("click", e => { //ボタンが押されたときに作動します
         e.preventDefault(); //ボタンを一旦無効化します
--- app/controllers/card_controller.rb
+++ app/controllers/card_controller.rb
@@ -13,10 +13,10 @@ class CardController < ApplicationController
       redirect_to action: "new"
     else
       customer = Payjp::Customer.create(
-      # description: '登録テスト', #なくてもOK
-      # email: current_user.email, #なくてもOK
+      description: '登録テスト', #なくてもOK
+      email: current_user.email, #なくてもOK
       card: params['payjp-token'],
-      # metadata: {user_id: current_user.id}
+      metadata: {user_id: current_user.id}
       ) #念の為metadataにuser_idを入れましたがなくてもOK
       @card = Card.new(user_id: current_user.id, customer_id: customer.id, card_id: customer.default_card)
       if @card.save
