140b32605228e2fda0aa159584a5a50bf863e028
kimura0823 <61189824+kimura0823@users.noreply.github.com>
2020-03-26 14:51:46 +0900

    server2 ok

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,6 @@
 class ProductsController < ApplicationController
-
+  before_action :set_product, only: [:edit, :show]
+  
   def index
     @products = Product.includes(:images).order('created_at DESC')
   end
@@ -42,11 +43,9 @@ class ProductsController < ApplicationController
   end
   
   def show
-    @product = Product.find(params[:id])
   end
   
   def edit
-    @product = Product.find(params[:id])
   end
   
   def update
