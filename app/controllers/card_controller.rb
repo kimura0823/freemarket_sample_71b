9a909538c98fda94c933b893f2883d2e74d3a0a0
shibatan1234 <61174383+shibatan1234@users.noreply.github.com>
2020-04-02 17:20:01 +0900

    create-kousin2

--- app/controllers/card_controller.rb
+++ app/controllers/card_controller.rb
@@ -13,16 +13,16 @@ class CardController < ApplicationController
       redirect_to action: "new"
     else
       customer = Payjp::Customer.create(
-      # description: '登録テスト', #なくてもOK
-      # email: current_user.email, #なくてもOK
+      description: '登録テスト', #なくてもOK
+      email: current_user.email, #なくてもOK
       card: params['payjp_token'],
       metadata: {user_id: current_user.id}
       ) #念の為metadataにuser_idを入れましたがなくてもOK
       @card = Card.new(user_id: current_user.id, customer_id: customer.id, card_id: customer.default_card)
-    if @card.save
-      redirect_to action: "show"
-    else
-      redirect_to action: "pay"
+      if @card.save
+        redirect_to action: "show"
+      else
+        redirect_to action: "pay"
       end
     end
   end
