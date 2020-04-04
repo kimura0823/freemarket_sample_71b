9365dc05c74c1273b09439ef75d13d204e2fb4aa
DON4024 <61001693+DON4024@users.noreply.github.com>
2020-04-04 19:44:32 +0900

    delete consol.log

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -3,20 +3,13 @@ $(document).on('turbolinks:load', function(){
   var searchWordList = $('.search_word_list').val();
 
 
-  function appendList(word, number) {
-    // console.log(word);
-    
-
+  function appendList(word, number) {    
     let item = $(`
     <li class="list result-list">
       <a href = "/products/${number}" class="search-word-list">
         <p>${word}</p>
-    
-    
-    
     `);
     // list.append(item);
-    // console.log(word);
     $("#result-word").append(item);
     // $("#result-word").text("");
     // $("#result-word").text(word);
@@ -28,31 +21,21 @@ $(document).on('turbolinks:load', function(){
   }
 
   $("#search-box").on("keyup", function() {
-    // console.log("ok")
     let input = $("#search-box").val();
     if (input==""){
       $("#result-word").empty();
     }else {
-      
       let inputs = input.split(" ");
-      // console.log(inputs);
       let newInputs = inputs.map(editElement);
       let reg = RegExp(newInputs.join("|"));
-      // console.log(reg);
       $(".list").remove();
   
       $.each( JSON.parse(searchWordList), function(i, word) {
-        // console.log(JSON.parse(searchWordList));
         if (word.match(reg)) {
           // var number = searchWordList.indexOf();
           appendList(word,i);
-          // console.log(i);
-
         }
         });
     };
-
-
-
   });
 });
