e956f165e6abe53f0a6911d13d27aac0cb58e47e
DON4024 <61001693+DON4024@users.noreply.github.com>
2020-04-05 15:43:42 +0900

    create searches controller

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -1,41 +1,46 @@
 $(document).on('turbolinks:load', function(){
   var list = $("#list");
   var searchWordList = $('.search_word_list').val();
-
-
+  
   function appendList(word, number) {    
     let item = $(`
     <li class="list result-list">
-      <a href = "/products/${number}" class="search-word-list">
+      <a href = "/products/${number+1}" class="search-word-list">
         <p>${word}</p>
     `);
-    // list.append(item);
     $("#result-word").append(item);
-    // $("#result-word").text("");
-    // $("#result-word").text(word);
   }
 
   function editElement(element) {
-    let result = "^" + element;
-    return result;
+    if (element != ""){
+      let result = "^" + element;
+      return result;
+    }else{
+      let result = "$^";
+      return result;
+    }
   }
-
-  $("#search-box").on("keyup", function() {
-    let input = $("#search-box").val();
+  
+  $("#_products_searches_keyword").on("keyup", function() {
+    let input = $("#_products_searches_keyword").val();
     if (input==""){
       $("#result-word").empty();
     }else {
       let inputs = input.split(" ");
       let newInputs = inputs.map(editElement);
       let reg = RegExp(newInputs.join("|"));
+      console.log(reg);
       $(".list").remove();
-  
       $.each( JSON.parse(searchWordList), function(i, word) {
         if (word.match(reg)) {
-          // var number = searchWordList.indexOf();
           appendList(word,i);
         }
         });
     };
+  });
+
+  $(".search_results_wrapper").on('click', function(){
+    console.log("ok")
+    $(".search_results_wrapper").empty();
   });
 });
