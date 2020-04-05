126d3dd8ad00ca00d9f940b6c2346302717f0ee8
DON4024 <61001693+DON4024@users.noreply.github.com>
2020-04-05 16:38:01 +0900

    Update searches_controller.rb

--- app/controllers/products/searches_controller.rb
+++ app/controllers/products/searches_controller.rb
@@ -1,9 +1,9 @@
 class Products::SearchesController < ApplicationController
   def index
     @search_results = Product.search(params[:keyword])
-    # @products = Product.where(judge: "出品中").order('created_at DESC').limit(3)
-    # @ladies_products = Product.where(judge: "sold").order('created_at DESC').limit(3)
-    # @images = Image.all
+    @products = Product.where(judge: "出品中").order('created_at DESC').limit(3)
+    @ladies_products = Product.where(judge: "sold").order('created_at DESC').limit(3)
+    @images = Image.all
     render 'products/index'
   end
 end
